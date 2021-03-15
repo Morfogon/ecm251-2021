8260dae1840bfb2a6ed17d86821411d694e15a23
Morfogon <matheus_rossini2008@hotmail.com>
2021-03-15 09:31:00 -0300

    Aula05
    Inicio do MauaBank

--- Aula05/src/souza/rossini/matheus/Conta.java
+++ Aula05/src/souza/rossini/matheus/Conta.java
@@ -1,40 +1,50 @@
 package souza.rossini.matheus;
-public class Conta {
-    //Atributos da classe
-    Cliente cliente;
-    double saldo;
-    int numero;
 
-    //Métodos da classe
-    void depositar(double valor){
-        this.saldo = this.saldo + valor;
+public class Conta {
+    //Atributos das classe
+    private Cliente cliente;
+    private double saldo;
+    private int numero;
+
+    public Conta(String nome, double saldoInicial, int numero) {
+        this.saldo = saldoInicial;
+        this.numero = numero;
+        this.cliente = new Cliente(nome);
 
     }
-    boolean sacar (double valor){
+
+    //Métodos da classe
+    public void depositar(double valor) {
+        //this.saldo = this.saldo + valor;
+        this.saldo += valor;
+    }
+
+    public boolean sacar(double valor) {
         if(valor <= this.saldo){
             this.saldo -= valor;
             return true;
         }
         return false;
     }
-    boolean transferirDinheiro(Conta destino, double valor){
+
+    public boolean transferirDinheiro(Conta destino, double valor) {
         if(this.sacar(valor)){
             destino.depositar(valor);
             return true;
         }
         return false;
-
     }
-    void visualizarSaldo(){
-        System.out.println("Saldo atual: R$" + this.saldo);
+
+
+    public double getSaldo() {
+        return this.saldo;
     }
 
     @Override
     public String toString() {
         return "Conta{" +
-                "cliente=" + cliente +
+                "cliente=" + cliente.toString() +
                 ", saldo=" + saldo +
                 ", numero=" + numero +
                 '}';
-    }
-}
+    }}
