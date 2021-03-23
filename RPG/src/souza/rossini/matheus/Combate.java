64179346547763b40a58a747762c51aa98d75274
Morfogon <matheus_rossini2008@hotmail.com>
2021-03-22 21:57:53 -0300

    RPG

--- RPG/src/souza/rossini/matheus/Combate.java
+++ RPG/src/souza/rossini/matheus/Combate.java
@@ -1,12 +1,76 @@
 package souza.rossini.matheus;
 
-public class Combate {
+import java.util.Scanner;
 
-    Combate(Jogador player, Inimigo npc){
-        while(npc.vida > 0){
-            npc.vida -= 10;                  // toma o dano
-            System.out.println(player.nome + " deu 10 de dano");         // printa o dano
-            System.out.println(npc.vida);
+public class Combate {
+    private Scanner scanner;
+    public Jogador player;
+    public Inimigo npc;
+
+    private void exibirMenu() {
+        System.out.println("O que deseja fazer?");
+        System.out.println("1. Bater");
+        System.out.println("2. Curar");
+        System.out.println("0. Desistir");
+    }
+
+    private void avaliarOpcao(int opcao) {
+        switch (opcao) {
+            case 0:                                     //sair
+                System.out.println(npc.nome + "venceu o combate!");
+                break;
+
+            case 1:                                     //bater
+                npc.vida -= 10;
+                System.out.println(player.nome + " deu 10 de dano");
+                break;
+            case 2:
+                if (player.vida < 50) {
+                    player.vida += 5;
+                }
+                break;
+
+
         }
     }
+
+    public Combate(Jogador player, Inimigo npc){
+        while (npc.vida < 0) {
+            exibirMenu();
+            int opcao = 1;
+            this.scanner = new Scanner(System.in);
+            opcao = scanner.nextInt();
+            avaliarOpcao(opcao);
+
+        }
+
+
+
+
+       // while(npc.vida > 0){
+          //  npc.vida -= 10;                  // toma o dano
+           // System.out.println(player.nome + " deu 10 de dano");         // printa o dano
+            //System.out.println(npc.vida);
+
+
+
+
+
+    }
+
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- RPG/src/souza/rossini/matheus/Main.java
+++ RPG/src/souza/rossini/matheus/Main.java
@@ -3,11 +3,11 @@ package souza.rossini.matheus;
 public class Main {
 
     public static void main(String[] args) {
-        Inimigo enemy = new Inimigo("Cleber", 50);
+        Inimigo enemy = new Inimigo("Dragão", 50);
         Jogador player = new Jogador("Matheus", 50);
         Combate duelo = new Combate(player, enemy);
-
     }
+
 
 }
 
